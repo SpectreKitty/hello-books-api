ab4235684f0b9dbe861d330d0057a12541186065
AstryBrana <astry.brana@gmail.com>
2024-11-02 13:01:04 -0400

    got to 07b-from-dict-refactor

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -7,20 +7,18 @@ books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 @books_bp.post("")
 def create_book():
     request_body = request.get_json()
-    title = request_body["title"]
-    description = request_body["description"]
     
-    new_book = Book(title=title, description=description)
+    try:    
+        new_book = Book.from_dict(request_body)
+    
+    except KeyError as error:
+        response = {"message":f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
     db.session.add(new_book)
     db.session.commit()
-
-    response = {
-        "id": new_book.id,
-        "title": new_book.title,
-        "description": new_book.description
-    }
     
-    return response, 201
+    return new_book.to_dict(), 201
 
 @books_bp.get("")
 def get_all_books():
@@ -37,41 +35,32 @@ def get_all_books():
     query = query.order_by(Book.id)
 
     books = db.session.scalars(query)
-    
-    # could also be written as:
-    # books = db.session.execute(query).scalars()
 
     books_response = []
+
     for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
+        books_response.append(book.to_dict())
+
     return books_response
 
 @books_bp.get("/<book_id>")
 def get_one_book(book_id):
     book = validate_book(book_id)
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-        }
+    return book.to_dict()
     
 def validate_book(book_id):
     try:
         book_id = int(book_id)
     except:
-        response = {"message": f"book {book_id} invalid"}
+        response = {"message": f"Book {book_id} invalid"}
         abort(make_response(response, 400))
 
     query = db.select(Book).where(Book.id == book_id)
     book = db.session.scalar(query)
 
     if not book:
-        response = {"message": f"book {book_id} not found"}
+        response = {"message": f"Book {book_id} not found"}
         abort(make_response(response, 404))
         
     return book
@@ -83,6 +72,7 @@ def update_book(book_id):
 
     book.title = request_body["title"]
     book.description = request_body["description"]
+
     db.session.commit()
     
     return Response(status=204,mimetype="application/json")
@@ -90,6 +80,7 @@ def update_book(book_id):
 @books_bp.delete("/<book_id>")
 def delete_book(book_id):
     book = validate_book(book_id)
+
     db.session.delete(book)
     db.session.commit()
 
